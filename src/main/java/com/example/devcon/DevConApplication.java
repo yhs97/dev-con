557573431253ea7e0a0b9a80520f7cae24a552a0
yhs97 <sarvaiyayash@gmail.com>
2021-01-14 19:19:32 +0530

    First Commit

---
+++ src/main/java/com/example/devcon/DevConApplication.java
@@ -0,0 +1,13 @@
+package com.example.devcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevConApplication.class, args);
+	}
+
+}
