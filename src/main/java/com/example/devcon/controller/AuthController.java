4af9691bc7a777df2e6824e3f31d9a5874dee138
yhs97 <sarvaiyayash@gmail.com>
2021-02-03 19:11:12 -0700

    Saved user to database for register funtionality

---
+++ src/main/java/com/example/devcon/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.devcon.controller;
+
+import com.example.devcon.dto.RegisterRequest;
+import com.example.devcon.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest){
+        authService.signUp(registerRequest);
+        return new ResponseEntity(OK);
+    }
+}
---
+++ src/main/java/com/example/devcon/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.devcon.service;
+
+import com.example.devcon.dto.RegisterRequest;
+import com.example.devcon.model.User;
+import com.example.devcon.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    //Constructor Injection better than field injection(Autowiring)
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signUp(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+    }
+    
+}
